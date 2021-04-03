662367d93dc02e33d6d18bf4af12e6db3bd38eb4
RIKO-tlov <bluebyrair@outlook.jp>
2021-04-03 01:21:58 +0000

    [start]5-4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7fd79a3973f5434418586ef41f508ec536c7b5765ff3b984e1a2b66639e04d228a30812399a4c6b65913e7144622a0bc0f7ba6e62e3f16580d4cd0277b196c29'
